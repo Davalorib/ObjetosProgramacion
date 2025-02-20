a2966a3b7f2f0c2f7489461cbaf388a2b75c3d53
A607\1DAW <davalorib@gmail.com>
2025-02-20 20:42:00 +0100

    si

--- Objetos/src/main/java/Herencia/Dispositivos/App.java
+++ Objetos/src/main/java/Herencia/Dispositivos/App.java
@@ -12,12 +12,49 @@ public class App {
         listaDispositivos.add(new AireAcondicionado("Nikelodeon"));
         listaDispositivos.add(new Televisor("Samsung"));
 
+
+        Dispositivo proyector = new Dispositivo("Electronik") {
+            @Override
+            public void encender() {
+                if (isEstado()) System.out.println("El proyector ya está encendido");
+                else{
+                    System.out.println("Encendiendo proyector con ajuste automático de brillo...");
+                    setEstado(true);
+                }
+            }
+        };
+
+
+        ControlRemoto proyector_control = new ControlRemoto() {
+            @Override
+            public void sincronizar() {
+                System.out.println("Sincronizando proyector con control remoto de presentación...");
+            }
+        };
+
+
+        Dispositivo horno = new Dispositivo("Balay jose") {
+            @Override
+            public void encender() {
+                if (isEstado()) System.out.println("El horno ya está encendido");
+                else {
+                    System.out.println("Calentando horno con ajuste automático de temperatura..");
+                    setEstado(true);
+                }
+            }
+        };
+
+
+        listaDispositivos.add(horno);
+        listaDispositivos.add(proyector);
+
+
         for (Dispositivo dispositivo:listaDispositivos){
             System.out.println("--------------------------");
             dispositivo.encender();
             if (dispositivo instanceof ControlRemoto) ((ControlRemoto) dispositivo).sincronizar();
+            else if (dispositivo.getNombre().equals("Electronik")) proyector_control.sincronizar();
             dispositivo.mostrarEstado();
-            dispositivo.apagar();
             dispositivo.apagar();
         }
 
